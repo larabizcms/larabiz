429dffeaae9d254ec018d8de530f9b5958c7358b
Juzaweb <dangtheanh16@gmail.com>
2024-05-18 13:16:26 +0700

    :+1: Swagger api documentation

--- resources/js/views/api/Document.tsx
+++ resources/js/views/api/Document.tsx
@@ -2,6 +2,22 @@ import SwaggerUI from "swagger-ui-react"
 import "swagger-ui-react/swagger-ui.css"
 import React from "react";
 
+interface Request {
+    [k: string]: any;
+}
+
+const requestInterceptor = (req: Request) => (
+    {
+        ...req,
+        headers: {
+            Accept: 'application/json'
+        }
+    }
+);
+
 export default function Document(): React.JSX.Element {
-    return <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />;
+    return <SwaggerUI
+        url={process.env.MIX_APP_URL + "/api/swagger.json"}
+        requestInterceptor={requestInterceptor}
+    />;
 }
