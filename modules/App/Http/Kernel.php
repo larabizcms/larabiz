f6f25897cfd10f6ac4ccfc39ab31f17d95b455f4
Juzaweb <dangtheanh16@gmail.com>
2024-05-18 11:56:52 +0700

    :+1: Admin middleware

--- modules/App/Http/Kernel.php
+++ modules/App/Http/Kernel.php
@@ -30,17 +30,22 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            //\LarabizCMS\Modules\App\Http\Middleware\EncryptCookies::class,
-            //\Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            //\Illuminate\Session\Middleware\StartSession::class,
-            //\Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            //\LarabizCMS\Modules\App\Http\Middleware\VerifyCsrfToken::class,
+            \LarabizCMS\Modules\App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \LarabizCMS\Modules\App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
         'api' => [
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
+        'admin-api' => [
+            'api',
+            \LarabizCMS\Core\Http\Middleware\Admin::class,
         ],
 
         'static' => [
@@ -67,5 +72,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \LarabizCMS\Core\Http\Middleware\Admin::class,
     ];
 }
