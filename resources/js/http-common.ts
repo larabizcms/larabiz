d8faffb39ba51c5e0ca76e3cca9ede28a8af8b16
Juzaweb <dangtheanh16@gmail.com>
2024-05-24 22:07:49 +0700

    :+1: Vite build

--- resources/js/http-common.ts
+++ resources/js/http-common.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const authToken = localStorage.getItem("lb_auth_token") ? JSON.parse(localStorage.getItem("lb_auth_token") as string) : null;
 
 export default axios.create({
-    baseURL: "/api",
+    baseURL: import.meta.env.VITE_APP_URL + "/api",
     headers: {
         "Content-type": "application/json",
         ...(authToken && { 'Authorization': `Bearer ${authToken.access_token}` }),
