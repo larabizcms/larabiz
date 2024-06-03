5763e7e0e4135e10f4cbfdd40facf0a08b9e9ef0
Juzaweb <dangtheanh16@gmail.com>
2024-06-03 22:05:16 +0700

    :+1: Loadable component

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,22 +1,22 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { RouterProvider } from 'react-router-dom';
 import { store } from '@/store';
 import { Provider } from 'react-redux';
 import router from '@/routes';
 import ThemeCustomization from '@larabiz/themes';
 import ScrollTop from '@larabiz/layouts/components/ScrollTop';
-import Loader from "@larabiz/layouts/components/Loader";
 
 export default function App() {
     return (
         <React.StrictMode>
             <Provider store={store}>
                 <ThemeCustomization>
-                    <Suspense fallback={<Loader />}>
+                    {/* Fix: A component suspended while responding to synchronous input. This will cause the UI to be replaced with a loading indicator... */}
+                    {/* <Suspense fallback={<Loader />}> */}
                         <ScrollTop>
                             <RouterProvider router={router} />
                         </ScrollTop>
-                    </Suspense>
+                    {/* </Suspense> */}
                 </ThemeCustomization>
             </Provider>
         </React.StrictMode>
