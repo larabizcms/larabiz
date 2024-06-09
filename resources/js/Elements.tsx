29bc8f574fa0f5af0bf3baf1acc9a93eb43ebeae
Juzaweb <dangtheanh16@gmail.com>
2024-06-09 16:18:04 +0700

    :+1: Element types

--- resources/js/Elements.tsx
+++ resources/js/Elements.tsx
@@ -2,7 +2,7 @@ import { ElementProps } from "@larabiz/views/ElementBuilder";
 import RootElements from "@larabiz/RootElements";
 
 const Elements = (props: ElementProps, index: number): JSX.Element|null => {
-    switch (props.config.element) {
+    switch (props.config?.element) {
         // Custom templates
         default:
             return RootElements(props, index);
