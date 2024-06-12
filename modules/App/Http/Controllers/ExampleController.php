b6a3532f801f0fc443e9ae950b2dce4e6b45b84e
Juzaweb <dangtheanh16@gmail.com>
2024-06-12 22:45:26 +0700

    update .gitignore, ExampleController.php and User.php

--- modules/App/Http/Controllers/ExampleController.php
+++ modules/App/Http/Controllers/ExampleController.php
@@ -24,15 +24,7 @@ class ExampleController extends Controller
 {
     public function index()
     {
-        Breadcrumb::items([
-            [
-                'title' => 'Dashboard',
-                'url' => '/admin-cp/dashboard',
-            ],
-            [
-                'title' => 'Example',
-            ],
-        ]);
+        Breadcrumb::add('Example');
 
         $input = TextField::make(['name' => 'textfield', 'label' => 'Textfield'])
             ->placeholder('Placeholder text');
@@ -43,23 +35,15 @@ class ExampleController extends Controller
             ->value('');
         $editor = Editor::make(['name' => 'editor', 'label' => 'Editor'])->value('<b>Hello</b>');
 
-        $card = Card::make(
-            [
-                //$text,
-                ContainerGrid::make()->add(
-                    $input,
-                    $textarea,
-                    $select,
-                    $editor,
-                ),
-            ]
-        )->title('Form');
+        $card = Card::make()->title('Form');
+        $card->add(ContainerGrid::make()->add(
+            $input,
+            $textarea,
+            $select,
+            $editor,
+        ));
         $col3 = ItemGrid::make()->xs(6)->add($card);
 
-        return Page::make(
-            [
-                ContainerGrid::make()->add($col3),
-            ]
-        )->fill(['title' => 'Example', 'description' => 'Example']);
+        return Page::make()->add(ContainerGrid::make()->add($col3))->fill(['title' => 'Example', 'description' => 'Example']);
     }
 }
--- modules/App/Models/User.php
+++ modules/App/Models/User.php
@@ -2,7 +2,18 @@
 
 namespace LarabizCMS\Modules\App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Notifications\DatabaseNotificationCollection;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection as BaseCollection;
+use LarabizCMS\Core\Contracts\Permissions\Permission as PermissionContract;
+use LarabizCMS\Core\Models\Permissions\Permission;
 use LarabizCMS\Core\Models\User as UserBase;
+use Laravel\Passport\Client;
+use Laravel\Passport\Token;
 
 /**
  * LarabizCMS\Modules\App\Models\User
@@ -10,34 +21,34 @@ use LarabizCMS\Core\Models\User as UserBase;
  * @property int $id
  * @property string $name
  * @property string $email
- * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property Carbon|null $email_verified_at
  * @property string $password
  * @property string|null $remember_token
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
  * @property string $uuid
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Passport\Client> $clients
+ * @property-read Collection<int, Client> $clients
  * @property-read int|null $clients_count
- * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read DatabaseNotificationCollection<int, DatabaseNotification> $notifications
  * @property-read int|null $notifications_count
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Passport\Token> $tokens
+ * @property-read Collection<int, Token> $tokens
  * @property-read int|null $tokens_count
- * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|User query()
- * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereUuid($value)
- * @method static Builder|User permission(\LarabizCMS\Core\Contracts\Permissions\Permission|\Illuminate\Support\Collection|array|string|int $permissions)
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \LarabizCMS\Core\Models\Permissions\Permission> $permissions
+ * @method static Builder|User newModelQuery()
+ * @method static Builder|User newQuery()
+ * @method static Builder|User query()
+ * @method static Builder|User whereCreatedAt($value)
+ * @method static Builder|User whereEmail($value)
+ * @method static Builder|User whereEmailVerifiedAt($value)
+ * @method static Builder|User whereId($value)
+ * @method static Builder|User whereName($value)
+ * @method static Builder|User wherePassword($value)
+ * @method static Builder|User whereRememberToken($value)
+ * @method static Builder|User whereUpdatedAt($value)
+ * @method static Builder|User whereUuid($value)
+ * @method static Builder|User permission(PermissionContract|BaseCollection|array|string|int $permissions)
+ * @property-read Collection<int, Permission> $permissions
  * @property-read int|null $permissions_count
- * @mixin \Eloquent
+ * @mixin Eloquent
  */
 class User extends UserBase
 {
