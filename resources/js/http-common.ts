750a8a178b178acb0d2732790cd764de78744137
Juzaweb <dangtheanh16@gmail.com>
2024-06-16 17:40:54 +0700

    feat: Implement request interceptor for seamless API authentication

--- resources/js/http-common.ts
+++ resources/js/http-common.ts
@@ -1,20 +1,31 @@
 import { apiBaseUrl } from "@larabiz/helpers/helper";
 import axios from "axios";
 
-function getDefaultHeaders() {
-    const authToken = localStorage.getItem("lb_auth_token")
-        ? JSON.parse(localStorage.getItem("lb_auth_token") as string)
-        : null;
-
-    return {
-        "Content-type": "application/json",
-        ...(authToken && { 'Authorization': `Bearer ${authToken.access_token}` }),
-    };
-}
-
-const http = (headers = []): { [key: string]: any } => axios.create({
+const http = axios.create({
     baseURL: apiBaseUrl,
-    headers: { ...getDefaultHeaders(), ...headers },
+    // headers: getDefaultHeaders(),
 });
+
+http.interceptors.request.use(
+    config => {
+        // Get the token from cookies
+        const authToken = localStorage.getItem("lb_auth_token")
+            ? JSON.parse(localStorage.getItem("lb_auth_token") as string)
+            : null; // Adjust according to your storage method
+
+        config.headers = config.headers || {};
+
+        if (authToken) {
+            // If the token exists, set the Authorization header
+            config.headers['Authorization'] = `Bearer ${authToken.access_token}`;
+        }
+
+        return config;
+    },
+    error => {
+        // Handle the error
+        return Promise.reject(error);
+    }
+);
 
 export default http;
