f1278b437c64eefa5ce17cf3c2108d435ddd9382
Thế Anh Đặng <dangtheanh16@gmail.com>
2024-08-11 10:02:53 +0700

    feat: Set up basic User model and factories
    This commit introduces the foundational User model, along with its corresponding factory and database seeder. This lays the groundwork for user management within the application.

---
+++ modules/App/Database/Seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace LarabizCMS\Modules\App\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \LarabizCMS\Modules\App\Models\User::factory(10)->create();
+
+        // \LarabizCMS\Mediable\Models\Media::factory(1)->create();
+    }
+}
