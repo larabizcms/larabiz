0558301ed2a1ca058cda457f1d98e88a6a527f5c
Thế Anh Đặng <dangtheanh16@gmail.com>
2024-08-11 10:24:04 +0700

    feat: Integrate Firebase for analytics tracking
    Initializes Firebase within the application, enabling analytics data collection for enhanced monitoring and insights. Streamlines language files and removes unused CSS styles for improved code maintainability.

--- resources/js/firebase.ts
+++ resources/js/firebase.ts
@@ -4,13 +4,7 @@ import { getAnalytics } from "firebase/analytics";
 // import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyApZoTLVnb9RgBunedX_eyZHrDvyIiLaOg",
-  authDomain: "larabiz-2fcf6.firebaseapp.com",
-  projectId: "larabiz-2fcf6",
-  storageBucket: "larabiz-2fcf6.appspot.com",
-  messagingSenderId: "153937459263",
-  appId: "1:153937459263:web:0e0b33ecde1e50486f608a",
-  measurementId: "G-99WSZWDQNB"
+  
 };
 
 // Initialize Firebase
