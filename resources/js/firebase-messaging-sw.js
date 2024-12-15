e1d12948cc1ef9fcea6ade28948f76a5f641a2ad
Thế Anh Đặng <dangtheanh16@gmail.com>
2024-12-15 15:52:41 +0700

    Simplifies Firebase Messaging setup
    Removes commented-out code for Firebase initialization when hosted on Firebase Hosting, as it's now using direct CDN URLs.  This streamlines the service worker setup and removes unnecessary configuration.
    
    Also removes a console log and comment for customizing notifications within the background message handler.

--- resources/js/firebase-messaging-sw.js
+++ resources/js/firebase-messaging-sw.js
@@ -1,24 +1,5 @@
-// Import and configure the Firebase SDK
-// These scripts are made available when the app is served or deployed on Firebase Hosting
-// If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-// importScripts('/__/firebase/9.2.0/firebase-app-compat.js');
-// importScripts('/__/firebase/9.2.0/firebase-messaging-compat.js');
-// importScripts('/__/firebase/init.js');
-
-/**
- * Here is is the code snippet to initialize Firebase Messaging in the Service
- * Worker when your app is not hosted on Firebase Hosting.
-
- // Give the service worker access to Firebase Messaging.
- // Note that you can only use Firebase Messaging here. Other Firebase libraries
- // are not available in the service worker.
-**/
 importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
-
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -27,16 +8,9 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-    // console.log(
-    //     '[firebase-messaging-sw.js] Received background message ',
-    //     payload
-    // );
-    // Customize notification here
     const notificationTitle = payload.notification.title;
 
     const notificationOptions = {
